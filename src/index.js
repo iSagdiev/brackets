54ed9d6b19bfef94bce0afb21247f84e636246fb
iSagdiev <jay.iskander@gmail.com>
2020-10-21 20:52:44 +0500

    init: uploading task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] === bracketsConfig[j][0]) {
+              stack.push(str[i])
+            } else if(str[i]=== bracketsConfig[j][1]) {
+              if (stack == []) { return false} else {
+                if(bracketsConfig[j].indexOf(stack[stack.length-1]) === 0) {
+                    stack.pop();
+                  } else return false;
+              }
+            }    
+        }
+        if (stack[stack.length-1] === '|' && stack[stack.length-2] === '|' || stack[stack.length-1] === '7' && stack[stack.length-2] === '7'|| stack[stack.length-1] === '8' && stack[stack.length-2] === '8') {
+            stack.pop();
+            stack.pop();
+          }  
+    }
+    if (stack && stack.length) {
+        return false;
+    } else {
+        return true;
+    }
 }
